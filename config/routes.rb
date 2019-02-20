a3046ce63c1181cb5ddfba782ef91cb936b62db3
Franzé Jr <franzejr@gmail.com>
2019-02-20 09:21:04 -0300

    Set up rubocop and fix offenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :admin do
-      resources :users
+    resources :users
 
-      root to: "users#index"
-    end
+    root to: 'users#index'
+  end
   devise_for :users
-  root to: "home#index"
+  root to: 'home#index'
 end
